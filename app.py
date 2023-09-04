1c0c332e9005a844567390a6c6ae3e9147acefc3
Claudia Hertzog <claudia@kentseatech.com>
2023-09-04 15:11:22 -0700

    changes made for digital ocean

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
-from whitenoise import WhiteNoise   #for serving static files on Heroku
 
 # Instantiate dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
 # Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
 server = app.server 
-
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
 # Define Dash layout
 def create_dash_layout(app):
